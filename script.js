598f45e20d96f3c0ac11b646f5a3b9349675c848
Arun Thomas <arunatoneway@gmail.com>
2025-07-10 01:05:14 +0530

    delete function to delete specific task by pressing delete button along it and completed function to mark specific task as complete

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 let create = document.getElementById("button-addon2")
 let taskField = document.getElementById("taskField")
+let disfil = document.getElementById("disfil")
+let taskcount = 0
 
 
 
@@ -14,28 +16,60 @@ create.addEventListener('click', () => {
     } else {
 
 
-        document.getElementById("disfil").innerHTML = tasktext;
-        document.getElementById("disfil").style.backgroundColor = "red";
+        disfil.innerHTML = tasktext;
+        disfil.style.backgroundColor = "red";
 
         let task = document.createElement("div")
         task.style.display = 'flex';
+        task.style.justifyContent ="center"
+        task.style.alignContent ="center"
+        task.style.textAlign ="center"
 
 
         let check = document.createElement("button")
         check.appendChild(document.createTextNode("done"))
+        check.style.height = "60px"
+
+        check.addEventListener('click', () => {
+
+            delebtn.style.display ="None"
+            check.style.display ="None"
+            task.style.backgroundColor ="green"
+
+        })
 
 
         let discrip = document.createElement("p")
         discrip.style.color = 'white';
         discrip.appendChild(document.createTextNode(tasktext))
+        discrip.style.width ="50vw"
+        discrip.style.border = "2px solid black"
+        discrip.style.paddingInline ="30px"
+        discrip.style.backgroundColor ="blue"
+        discrip.style.height = "60px"
+        discrip.style.alignContent ="center"
 
 
         let delebtn = document.createElement("button")
         delebtn.appendChild(document.createTextNode("Delete"))
+        delebtn.style.height = "60px"
+
+        delebtn.addEventListener('click', () => {
+
+            task.style.display ="None"
+
+        })
+
+
+
+
         document.body.appendChild(task)
         task.appendChild(check)
         task.appendChild(discrip)
         task.appendChild(delebtn)
+
+        taskField.value = ""
+        taskField.placeholder = "Enter your task here";
 
 
 
@@ -45,5 +79,7 @@ create.addEventListener('click', () => {
 
 
 })
+
+
 
 
