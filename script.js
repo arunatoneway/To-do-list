b0647065781e22b1bb8dce91c459dc9e15c6e888
Arun Thomas <arunatoneway@gmail.com>
2025-07-09 10:46:12 +0530

    check btn, task disc and delete btn are now showing in inline inside the task div and added if for displaying error message if there was nothing entered in task field

--- script.js
+++ script.js
@@ -1,32 +1,48 @@
 
 let create = document.getElementById("button-addon2")
+let taskField = document.getElementById("taskField")
+
 
 
 create.addEventListener('click', () => {
 
-    let tasktext = document.getElementById("taskField").value;
+    let tasktext = taskField.value;
+
+    if (!tasktext) {
+        taskField.placeholder = "Enter a task for creation";
+
+    } else {
 
 
-    document.getElementById("disfil").innerHTML = tasktext;
-    document.getElementById("disfil").style.backgroundColor = "red";
+        document.getElementById("disfil").innerHTML = tasktext;
+        document.getElementById("disfil").style.backgroundColor = "red";
 
-    let task = document.createElement("div")
-    let check = document.createElement("button")
-    check.appendChild(document.createTextNode("done"))
-    let discrip = document.createElement("p")
-    discrip.appendChild(document.createTextNode(tasktext))
-    let delebtn = document.createElement("button")
-    delebtn.appendChild(document.createTextNode("Delete"))
-    document.body.appendChild(task)
-    task.appendChild(check)
-    task.appendChild(discrip)
-    task.appendChild(delebtn)
-    
-
-    
+        let task = document.createElement("div")
+        task.style.display = 'flex';
 
 
-    
+        let check = document.createElement("button")
+        check.appendChild(document.createTextNode("done"))
+
+
+        let discrip = document.createElement("p")
+        discrip.style.color = 'white';
+        discrip.appendChild(document.createTextNode(tasktext))
+
+
+        let delebtn = document.createElement("button")
+        delebtn.appendChild(document.createTextNode("Delete"))
+        document.body.appendChild(task)
+        task.appendChild(check)
+        task.appendChild(discrip)
+        task.appendChild(delebtn)
+
+
+
+
+    }
+
+
 
 })
 
